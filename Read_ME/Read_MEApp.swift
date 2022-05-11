281ee824736dc3d53e5e85495379f29640997b14
jooeun kim <jay311311@gmail.com>
2022-05-11 14:05:59 +0900

    Initial Commit

---
+++ Read_ME/Read_MEApp.swift
@@ -0,0 +1,17 @@
+//
+//  Read_MEApp.swift
+//  Read_ME
+//
+//  Created by Jooeun Kim on 2022/05/11.
+//
+
+import SwiftUI
+
+@main
+struct Read_MEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
